a1eee9cefb28b88c8b443b009348329f54d66a25
gev-hobosyan <hobosyangevorg2008@gmail.com>
2026-05-17 13:11:58 +0400

    Added more windows and resizing.

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,34 @@
+"use client";
 import Window from "@/components/Window";
+import { useState } from "react";
+
+const WINDOWS = [
+	{ id: 1, title: "Finder", х: 100, y: 90, height: 500, width: 700 },
+	{ id: 2, title: "Terminal", х: 200, y: 100, height: 500, width: 700 },
+	{ id: 3, title: "Editor", х: 500, y: 230, height: 400, width: 1000 },
+];
 
 export default function Home() {
+	const [focusedWindow, setFocusedWindow] = useState(1);
+
+	console.log(focusedWindow);
+
 	return (
 		<div className="relative font-sans bg-black">
-			<Window
-				title={"Projects"}
-				x={100}
-				y={90}
-				height={500}
-				width={700}
-			></Window>
+			{WINDOWS.map((window) => (
+				<Window
+					key={window.id}
+					title={window.title}
+					x={window.х}
+					y={window.y}
+					height={window.height}
+					width={window.width}
+					focused={focusedWindow === window.id}
+					onFocus={() => setFocusedWindow(window.id)}
+				>
+					<></>
+				</Window>
+			))}
 		</div>
 	);
 }
--- components/Window.tsx
+++ components/Window.tsx
@@ -1,7 +1,7 @@
 "use client";
 import {
-	MouseEvent,
 	MouseEventHandler,
+	ReactNode,
 	useEffect,
 	useRef,
 	useState,
@@ -16,6 +16,10 @@ interface Props {
 	width: number;
 	minHeight?: number;
 	minWidth?: number;
+	children: ReactNode;
+	className?: string;
+	focused: boolean;
+	onFocus: () => void;
 }
 
 export default function Window({
@@ -26,16 +30,23 @@ export default function Window({
 	width,
 	minHeight = 300,
 	minWidth = 400,
+	className = "",
+	focused,
+	onFocus,
+	children,
 }: Props) {
 	const [pos, setPos] = useState({ x: x, y: y });
 	const [size, setSize] = useState({ w: width, h: height });
 
 	const [dragging, setDragging] = useState(false);
+	const [resizing, setResizing] = useState(false);
 	const dragStart = useRef<{
 		mx: number;
 		my: number;
-		ox: number;
-		oy: number;
+		ox?: number;
+		oy?: number;
+		ow?: number;
+		oh?: number;
 	} | null>(null);
 
 	const onTitleMouseDown: MouseEventHandler<HTMLDivElement> = (event) => {
@@ -50,22 +61,43 @@ export default function Window({
 		};
 	};
 
+	const onResizeMouseDown: MouseEventHandler<HTMLDivElement> = (event) => {
+		event.preventDefault();
+		event.stopPropagation();
+
+		setResizing(true);
+		dragStart.current = {
+			mx: event.clientX,
+			my: event.clientY,
+			oh: size.h,
+			ow: size.w,
+		};
+	};
+
 	useEffect(() => {
 		const onMove = (event: globalThis.MouseEvent) => {
 			if (dragging && dragStart.current) {
 				const dx = event.clientX - dragStart.current.mx;
 				const dy = event.clientY - dragStart.current.my;
 				setPos({
-					x: dragStart.current.ox + dx,
-					y: dragStart.current.oy + dy,
+					x: dragStart.current.ox! + dx,
+					y: dragStart.current.oy! + dy,
 				});
+			}
+			if (resizing && dragStart.current) {
+				const dw = event.clientX - dragStart.current.mx;
+				const dh = event.clientY - dragStart.current.my;
 
-				console.log("Move");
+				setSize({
+					w: Math.max(minWidth, dragStart.current.ow! + dw),
+					h: Math.max(minHeight, dragStart.current.oh! + dh),
+				});
 			}
 		};
 		const onUp = (event: globalThis.MouseEvent) => {
-			if (dragging && dragStart.current) {
+			if ((dragging || setResizing) && dragStart.current) {
 				setDragging(false);
+				setResizing(false);
 				dragStart.current = null;
 			}
 		};
@@ -77,30 +109,40 @@ export default function Window({
 			window.removeEventListener("mousemove", onMove);
 			window.removeEventListener("mouseup", onUp);
 		};
-	}, [dragging]);
+	}, [dragging, minHeight, minWidth, resizing]);
 
 	return (
 		<>
 			<div
-				className="bg-gray-900 absolute rounded-2xl"
+				className={`bg-gray-900 absolute rounded-2xl overflow-visible ${className}`}
 				style={{
 					width: size.w,
 					height: size.h,
 					top: pos.y,
 					left: pos.x,
-					zIndex: 20,
+					zIndex: focused ? 50 : 20,
 				}}
+				onClick={onFocus}
 			>
-				<div
-					className="relative w-full h-8"
-					onMouseDown={onTitleMouseDown}
-				>
+				<div className="relative w-full h-8" onMouseDown={onTitleMouseDown}>
 					<ControllWindow x={10} y={10}></ControllWindow>
 					<div className="absolute left-20 top-1.5 text-md font-bold">
 						<p>{title}</p>
 					</div>
 				</div>
 			</div>
+			<div
+				className="absolute rounded-2xl"
+				onMouseDown={onResizeMouseDown}
+				style={{
+					width: size.w + 10,
+					height: size.h + 10,
+					top: pos.y - 5,
+					left: pos.x - 5,
+					zIndex: 10,
+					cursor: "nwse-resize",
+				}}
+			></div>
 		</>
 	);
 }
